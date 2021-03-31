536755ba335530496ee767036554822bf82e494e
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-03-31 17:16:58 +0900

    3.31 수업

--- app/src/main/java/com/example/Kotlin/19.homework.kt
+++ app/src/main/java/com/example/Kotlin/19.homework.kt
@@ -16,6 +16,13 @@ fun main(array: Array<String>) {
     println(calculator2.divide(3, 5, 2, 4, 1))
     println()
 
+    //1.3
+    val calculator3: Calculator3 = Calculator3(3)
+    println(calculator3.plus(3).minus(3))
+    //-> 체이닝 (chaining)이 가능합니다.
+    //--------------------------------/Calculator(6)
+    //--------------------------------------------------/Cal(6).minus(3)
+
 
     //2.
     val bank: Bank = Bank("박상준", "1995/11/03", 100000)
@@ -23,6 +30,16 @@ fun main(array: Array<String>) {
     println(bank.withdraw(100000000))
     println(bank.deposit(100000))
     println()
+
+    val bank2: Bank = Bank("박상준", "1995/11/03", -10100)
+    println(bank2.checkBalance())
+    println("뱅크2")
+
+
+    //2.3무조건 1000원을 가지고 시작하는 계좌입니다.
+    val bank3 :Bank2 = Bank2("박상준","1995/11/03")
+    println(bank3.checkBalance())
+    println("뱅크3")
 
     //3.
 
@@ -90,8 +107,47 @@ class Calculator2() {
     }
 }
 
+//1.3)사칙연산 3단계
+class Calculator3(val initialValue: Int) {
+    fun plus(number: Int): Calculator3 {
+        val result = this.initialValue + number
+        return Calculator3(result)
+    }
+
+    fun minus(number: Int): Calculator3 {
+        val result = this.initialValue - number
+        return Calculator3(result)
+    }
+
+    fun multiply(number: Int): Calculator3 {
+        val result = this.initialValue * number
+        return Calculator3(result)
+    }
+
+    fun divide(number: Int): Calculator3 {
+        val result = this.initialValue / number
+        return Calculator3(result)
+    }
+
+}
+
 //2)은행 계좌 만들기
-class Bank(val name: String, val birth: String, var balance: Int) {
+class Bank {
+    val name: String
+    val birth: String
+    var balance: Int
+
+    constructor(name: String, birth: String, balance: Int) {
+        this.name = name
+        this.birth = birth
+        if (balance >= 0) {
+            this.balance = balance
+        } else {
+            this.balance = 0
+        }
+
+    }
+
     fun checkBalance(): Int {
         return balance
     }
@@ -110,6 +166,39 @@ class Bank(val name: String, val birth: String, var balance: Int) {
 
     fun deposit(Dmoney: Int): Int {
         return balance + Dmoney
+    }
+}
+
+//2)1000원가지고 시작하는 계좌만들기
+class Bank2(val name: String, val birth: String, var balance: Int =1000) {
+    fun checkBalance(): Int {
+        return balance
+    }
+
+    fun withdraw(Wmoney: Int): Int {
+        if (balance >= Wmoney) {
+            balance -= Wmoney
+            return balance
+        } else {
+            println("잔액없자나")
+            return balance
+
+        }
+
+    }
+
+    fun deposit(Dmoney: Int): Int {
+        return balance + Dmoney
+    }
+
+}
+
+//2.3))
+class Bank3(initialBalance: Int){
+    val balance:Int = if (initialBalance>=0) initialBalance else 0
+
+    fun checkBalance():Int{
+        return balance
     }
 }
 
---
+++ app/src/main/java/com/example/Kotlin/21.tv_class.kt
@@ -0,0 +1,64 @@
+package com.example.Kotlin
+
+fun main(args: Array<String>) {
+    val tv = Tv(listOf<String>("K", "M", "S"))
+    tv.channelDown()
+    println(tv.checkCurrentChannel())
+    tv.channelUp()
+    println(tv.checkCurrentChannel())
+    tv.channelUp()
+    println(tv.checkCurrentChannel())
+    tv.channelUp()
+    println(tv.checkCurrentChannel())
+    tv.channelUp()
+    println(tv.checkCurrentChannel())
+}
+
+class Tv(val channels: List<String>) {
+
+    var onOrOff: Boolean = false //True ->On, False ->Off
+    var currentChannelNumber = 0
+        set(value){
+            field = value
+            if (field>2){
+                field =0
+            }
+            if (field <0){
+                field =2
+            }
+        }
+        get(){
+            println("호출되었습니다")
+            return  field
+        }
+
+    fun switch() { //변수 기능은 소문자 fn ,class는 대문자로 적어주자
+        onOrOff = !onOrOff
+    }
+
+    fun checkCurrentChannel(): String { //현재 채널 방송사를 출력합니다.
+        return channels[currentChannelNumber]//채널의 밸류를 출력하기위해서  .."M사", "S사---등..
+    }
+
+    fun channelUp() { //채널 업
+        currentChannelNumber += 1
+
+//        channels.forEachIndexed { i, v ->
+//            if (currentChannelNumber == i) { //currentChannelNumber는 채널의 인덱스 입니다.
+//                currentChannelNumber += 1
+//                return
+//            }
+    }
+
+
+    fun channelDown() { //채널 다운.
+        currentChannelNumber -= 1
+
+//        channels.forEachIndexed { i, v ->
+//            if (currentChannelNumber == i) { //현재 채널 인덱스값이 i와 같다면
+//                currentChannelNumber -= 1
+//                return
+//            }
+    }
+}
+
