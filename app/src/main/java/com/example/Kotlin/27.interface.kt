f2b581db7c2691d866d23cc5d46e24087b7b1b52
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-04-01 21:52:05 +0900

    4.1

---
+++ app/src/main/java/com/example/Kotlin/27.interface.kt
@@ -0,0 +1,87 @@
+package com.example.Kotlin
+
+//27.Interface
+//-인터페이스는 약속이다. >>니가 이것을 구현하면 너도 이 타입이다!
+//-생성자가 없다 ->인스턴스화 시킬 수 없다 -> 설명서가 아니다.
+//-'지침서'입니다. >>이것을 구현하고 싶다면 반드시 아래 기능을 구현해라!
+
+//상속과 다른점
+// - 상속은 조상을 찾아가는 느낌
+// - 인터페이스는 종의 특징
+
+fun main(args:Array<String>){
+    val student_ :Student_ = Student_()
+
+
+    student_.eat()
+    student_.sleep()
+}
+
+
+interface Person_ {
+    fun eat()
+    fun sleep()
+}
+
+
+class Student_ : Person_ {
+    override fun eat() {
+
+    }
+
+    override fun sleep() {
+
+    }
+}
+
+class SoccerPlayer : Person_{
+
+    override fun eat() {
+
+    }
+
+    override fun sleep() {
+
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+open class Person() {
+    open fun eat() {
+
+    }
+
+    fun sleep() {
+
+    }
+}
+
+class Student() : Person() {
+
+}
+
+class Teacher() : Person() {
+
+}
