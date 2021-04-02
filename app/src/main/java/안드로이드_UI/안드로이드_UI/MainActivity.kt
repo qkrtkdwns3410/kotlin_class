3ee7eaacc65678005821dee73f092dfad6849137
qkrtkdwns3410 <qkrtkdwns3410@naver.com>
2021-04-02 18:35:38 +0900

    4.2 앱개발 시작단계 레이아웃..

---
+++ app/src/main/java/안드로이드_UI/안드로이드_UI/MainActivity.kt
@@ -0,0 +1,12 @@
+package 안드로이드_UI.안드로이드_UI
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.Kotlin.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
